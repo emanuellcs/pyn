140c00b6d5a03df561a042909c673cff4dc0bcbe
Emanuel Lázaro <120259086+emanuellcs@users.noreply.github.com>
2025-01-08 15:50:39 -0300

    Add missing command to install project requirements

--- setups/setup_linux.sh
+++ setups/setup_linux.sh
@@ -9,3 +9,6 @@ python3 -m venv venv
 
 # Activate the virtual environment
 source venv/bin/activate
+
+# Install dependencies from requirements.txt in the script directory
+pip install -r "$SCRIPT_DIR/requirements.txt"
